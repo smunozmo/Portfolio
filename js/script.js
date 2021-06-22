0e330e4af84074cf8221a90d6e073e210ec00569
Sebastián M <smunozmo@gmail.com>
2021-06-22 12:28:37 -0500

    Fix linters errors for JS.

--- js/script.js
+++ js/script.js
@@ -3,17 +3,16 @@ const closeMenu = document.querySelector('.menu-close');
 const openMenu = document.querySelector('.menu-open');
 const linkMenu = document.querySelectorAll('.menu a');
 
-openMenu.addEventListener('click', menuOpen);
-closeMenu.addEventListener('click', menuClose);
-
-
-function menuOpen(){
-    containerMenu.style.display = 'flex';
+function menuOpen() {
+  containerMenu.style.display = 'flex';
 }
-function menuClose(){
-    containerMenu.style.display = 'none';
+function menuClose() {
+  containerMenu.style.display = 'none';
 }
 
 linkMenu.forEach((link) => {
-    link.addEventListener('click', menuClose)
+  link.addEventListener('click', menuClose);
 });
+
+openMenu.addEventListener('click', menuOpen);
+closeMenu.addEventListener('click', menuClose);
