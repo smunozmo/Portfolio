aa230e37812a26bd8b943aa33c9c88506c1cf687
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-06-22 17:02:06 +0100

    Add anchor link

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const containerMenu = document.querySelector('.menu');
 const closeMenu = document.querySelector('.menu-close');
 const openMenu = document.querySelector('.menu-open');
+const linkMenu = document.querySelectorAll('.menu a');
 
 openMenu.addEventListener('click', menuOpen);
 closeMenu.addEventListener('click', menuClose);
+
 
 function menuOpen(){
     containerMenu.style.display = 'flex';
@@ -11,3 +13,7 @@ function menuOpen(){
 function menuClose(){
     containerMenu.style.display = 'none';
 }
+
+linkMenu.forEach((link) => {
+    link.addEventListener('click', menuClose)
+});
